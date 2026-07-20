4c4b52a3a1419ff720293e9a0c9aa86f8d306245
DaVor05 <davidvid1201@gmail.com>
2026-07-20 01:12:25 -0700

    Paso 13 al 16: Mostrar QR, Regular el Brillo, Estado de la App y Icono de la Camara

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -2,41 +2,51 @@ import { NgTemplateOutlet } from '@angular/common';
 import { CouponService } from './../../services/coupon.service';
 import { Component, inject } from '@angular/core';
 
-
-import { IonHeader, IonToolbar, 
-  IonTitle, IonContent, IonLabel, IonSegment, IonSegmentButton,
-  IonSegmentContent, IonSegmentView, IonCard, IonCardHeader, 
-  IonCardSubtitle, IonCardTitle, IonImg, IonCol, IonGrid, IonRow } from '@ionic/angular/standalone';
+import { 
+  IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonSegment, 
+  IonSegmentButton, IonSegmentContent, IonSegmentView, IonCard, 
+  IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg, IonCol, 
+  IonGrid, IonRow, IonIcon, IonItem, IonButtons, IonButton
+} from '@ionic/angular/standalone';
 
 import { Coupon } from 'src/app/models/coupon.model';
-import { FilterCouponCategoryPipe} from 'src/app/pipes/filter-coupon-category-pipe';
-
+import { FilterCouponCategoryPipe } from 'src/app/pipes/filter-coupon-category-pipe';
+import { addIcons } from 'ionicons';
+import { cameraOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent,
-    IonLabel, IonSegment, IonSegmentButton, IonSegmentContent, 
-    IonSegmentView, FilterCouponCategoryPipe, IonCard,
-    IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg, IonCol, 
-    IonGrid, IonRow, NgTemplateOutlet
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonSegment, 
+    IonSegmentButton, IonSegmentContent, IonSegmentView, FilterCouponCategoryPipe, 
+    IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg, IonCol, 
+    IonGrid, IonRow, NgTemplateOutlet, IonIcon, IonItem, IonButtons, IonButton
   ],
 })
 export class Tab1Page {
   
   private couponService: CouponService = inject(CouponService);
-  coupons:Coupon[]=[];
+  coupons: Coupon[] = [];
 
-  async ionViewWillEnter(){
-    this.coupons = await this.couponService.getCoupons();
-    console.log(this.coupons)
-
+  constructor() {
+    // Registramos el icono para poder usarlo en standalone
+    addIcons({ cameraOutline });
   }
 
-   changeActive(coupon: Coupon){
+  async ionViewWillEnter() {
+    this.coupons = await this.couponService.getCoupons();
+    console.log(this.coupons);
+  }
+
+  changeActive(coupon: Coupon) {
     coupon.active = !coupon.active;
     this.couponService.saveCoupons(this.coupons);
   }
 
+  startCamera() {
+    console.log('Abriendo cámara para escanear...');
+    // Aquí irá tu lógica para abrir la cámara de la cuponera más adelante
+  }
 }
--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,14 +1,65 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonText } from '@ionic/angular/standalone';
+import { Platform } from '@ionic/angular';
+import { QRCodeComponent } from 'angularx-qrcode';
+import { ScreenBrightness, GetBrightnessReturnValue } from '@capacitor-community/screen-brightness';
+import { App } from '@capacitor/app';
+import { CouponService } from '../../services/coupon.service';
+import { Coupon } from 'src/app/models/coupon.model';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent, 
+    IonGrid, IonRow, IonCol, IonText, 
+    QRCodeComponent
+  ]
 })
 export class Tab2Page {
 
+  private couponService: CouponService = inject(CouponService);
+  private platform: Platform = inject(Platform);
+  
+  QRCode!: string;
+  private currentBrightness!: GetBrightnessReturnValue;
+
   constructor() {}
 
+  async ionViewWillEnter() {
+    if (!this.platform.is('desktop')) {
+      this.currentBrightness = await ScreenBrightness.getBrightness();
+      this.setMaxBrightness();
+
+      if (this.platform.is('ios')) {
+        App.addListener('appStateChange', (state) => {
+          if (state.isActive) {
+            this.setMaxBrightness();
+          } else {
+            this.restoreBrightness();
+          }
+        });
+      }
+    }
+
+    const coupons: Coupon[] = await this.couponService.getCoupons();
+    const couponsActive: Coupon[] = coupons.filter((coupon: Coupon) => coupon.active);
+    this.QRCode = couponsActive.length > 0 ? JSON.stringify(couponsActive) : '';
+  }
+
+  ionViewDidLeave() {
+    if (!this.platform.is('desktop')) {
+      this.restoreBrightness();
+      App.removeAllListeners();
+    }
+  }
+
+  setMaxBrightness() {
+    ScreenBrightness.setBrightness({ brightness: 1 });
+  }
+
+  restoreBrightness() {
+    ScreenBrightness.setBrightness({ brightness: this.currentBrightness.brightness });
+  }
 }
